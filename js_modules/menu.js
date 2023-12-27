48eb588fc62713425896564a04704e311dc4ad3e
Kristin <89033364+kriscodesstuff@users.noreply.github.com>
2023-12-27 14:03:29 -0600

    Finish layout for menu tab

--- js_modules/menu.js
+++ js_modules/menu.js
@@ -6,7 +6,16 @@ const menuName = document.createElement('h1');
 const p = document.createElement('p');
 const img = document.createElement('img');
 
+h1.innerText = 'Menu';
+menuName.innerText = 'Creme Brulee';
+p.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rhoncus.'
+img.src = '../imgs/maple-creme-brulee-1200-50.jpg';
 
+div.appendChild(h1);
+div.appendChild(menuCard);
+menuCard.appendChild(menuName);
+menuCard.appendChild(p);
+menuCard.appendChild(img);
 }
 
 export { menu };
